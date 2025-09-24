a1eaf00512790d15d650b567c2376067275b9c24
justin !! <jzou0417a@gmail.com>
2025-09-24 09:51:44 -0400

    Modularize components

--- src/client/RA_Client/Bootstrapper.client.lua
+++ src/client/RA_Client/Bootstrapper.client.lua
@@ -4,16 +4,18 @@ duration of play
 ]]
 
 local dir = require(game.ReplicatedStorage.Shared.RA_Directory)
-local maid = require(dir.Modules.Utility.Maid).new()
+local maid = require(dir.Utility.Maid).new()
 local ProjectileController = require(dir.Modules.Core.ProjectileController)
-local TurretController = require(dir.Modules.Core.TurretController)
+local TurretController = require(dir.Modules.Components.TurretController)
 local signals = require(dir.Signals)
 
 local net, evts = dir.GetNetwork()
 -- load order
 
 net:Connect(evts.OnSeated, function(required)
-    maid:GiveTask(TurretController.new({}, required))
+    maid:GiveTask(TurretController.new({
+        rotArgs = {}
+    }, required))
 end)
 
 net:Connect(evts.OnUnseated, function(required)
